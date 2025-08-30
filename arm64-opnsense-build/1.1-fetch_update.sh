7fd20900e5a1abb473954974be3cc3322dc85138
Martina Pietruschka <martinapietruschka@MacBook-Pro-von-Martina.local>
2025-08-30 03:10:26 +0200

    plugin-extras

--- arm64-opnsense-build/1.1-fetch_update.sh
+++ arm64-opnsense-build/1.1-fetch_update.sh
@@ -34,6 +34,9 @@ cd $CURRENT_DIR
 echo "Copy R5S conf files"
 cp $SRC_DIR/R6S.conf $SRC_DIR/R5S_USB.conf $SRC_DIR/R5S_UBOOT.conf $SRC_DIR/R5S_EDK2.conf /usr/tools/device
 
+echo "Copy plugins-extra files"
+cp -rf plugins-extra /usr/tools/
+
 #echo "Copy custom rc file"
 #cp $SRC_DIR/usr-core-src-etc-rc /usr/core/src/etc/rc
 
